255b87810319bfae303b7725ffa68ecf2e80aab8
Phalyaj5119 <phalyajhamb@icloud.com>
2023-03-19 21:01:26 +0530

    add anagram

---
+++ anagram page.py
@@ -0,0 +1,29 @@
+print("                                                     ANAGRAMS ")
+print("                                             HOW TO PLAY ANAGRAMS ?! ")
+print("                                              IT IS A TWO PLAYER GAME")
+print("                                     THE PLAYERS CAN CHOOSE THE DIFFICULTY LEVEL ")
+print("                                                 EASY/MEDIUM/HARD")
+print("       EACH PLAYERS WILL BE PROVIDED SIX DISTINCT LETTERS AND THEY HAVE TO FORMULATE MEANINGFUL WORDS USING THE GIVEN LETTERS")
+print("                                  FOR A THREE LETTER WORD, 100 POINTS WILL BE AWARDED")
+print("                                  FOR A FOUR LETTER WORD, 200 POINTS WILL BE AWARDED")
+print("                                  FOR A FIVE LETTER WORD, 400 POINTS WILL BE AWARDED")
+print("                                  FOR A six LETTER WORD, 1200 POINTS WILL BE AWARDED")
+print("                                     THE PLAYER WITH THE HIGHEST SCORE POINTS WINS")
+print("                                                   GOOD LUCK!!!")
+
+
+
+cho=input("ENTER UR OPTION: ")
+print("")
+if cho=='easy':
+    print("yes")    
+elif cho=='EASY':
+    print("yes1")    
+elif cho=='medium': 
+    print("yes2")
+elif cho=='MEDIUM':
+    print("yes3")
+elif cho=='HARD':
+   print("yes4")
+else:
+   print("yes5")
